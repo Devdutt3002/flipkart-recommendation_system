8bede072b73ff6cb8dd131d1f8cc993662f75e0d
Devdutt3002 <devduttk30@gmail.com>
2025-11-10 20:27:09 +0530

    Use environment variable for server port
    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def chat():
 
       return str(result)
 
-if __name__ == '__main__':
-    
-    app.run(host="0.0.0.0", port=5000, debug= True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
